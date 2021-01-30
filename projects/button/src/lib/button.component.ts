39d6d9c58cc88f1517123c14675f6b9fa209b8f6
novakand <a79264799999@gmail.com>
2021-01-30 11:32:36 +0300

    fix: run prettier over the entire codebase

--- projects/button/src/lib/button.component.ts
+++ projects/button/src/lib/button.component.ts
@@ -12,5 +12,7 @@ import { Component, OnInit } from "@angular/core";
 export class ButtonComponent implements OnInit {
   constructor() {}
 
-  ngOnInit() {  }
+  ngOnInit() { 
+    console.log('')
+   }
 }
